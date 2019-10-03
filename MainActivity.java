9eb9997be322830c13a605da5da572678a0f1a6b
asmita910 <55904719+asmita910@users.noreply.github.com>
2019-10-03 12:29:33 +0530

    Main Activity
    Main Activity including four bottom tabs with fragments for each (having four classes as assigned i.e. Public ,Class,Resolved nad Solved),a search bar and a menu consisting of 3 options which are Raise a complaint,Settings and Contact Us.

---
+++ MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.jiet_mainactivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView mMainBottomNavBar;
+    private FrameLayout mMainFrame;
+    private PublicFragment mpublicFragment;
+    private ClassFragment mclassFragment;
+    private ResolvedFragment mresolvedFragment;
+    private SolvedFragment msolvedFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMainFrame=(FrameLayout)findViewById(R.id.MainFrame);
+        mMainBottomNavBar=(BottomNavigationView)findViewById(R.id.MainBottomNavBar);
+
+        mpublicFragment = new PublicFragment();
+        mclassFragment = new ClassFragment();
+        mresolvedFragment = new ResolvedFragment();
+        msolvedFragment = new SolvedFragment();
+
+
+        mMainBottomNavBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_Public :
+                        mMainBottomNavBar.setItemBackgroundResource(R.color.colorPrimary);
+                        setFragment(mpublicFragment);                     //method to shift from one fragment to another
+                        return true;
+
+                    case R.id.nav_Class :
+                        mMainBottomNavBar.setItemBackgroundResource(R.color.colorAccent);
+                        setFragment(mclassFragment);
+                        return true;
+
+                    case R.id.nav_Resolved  :
+                        mMainBottomNavBar.setItemBackgroundResource(R.color.colorPrimaryDark);
+                        setFragment(mresolvedFragment);
+                        return true;
+
+                    case R.id.nav_Solved  :
+                        mMainBottomNavBar.setItemBackgroundResource(R.color.design_default_color_primary_dark);
+                        setFragment(msolvedFragment);
+                        return true;
+
+                        default:return false;
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.dot_popup_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.raise_a_complaint_menuItem :
+                Toast.makeText(this,"Complaint raised selected",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.settings_menuItem  :
+                Toast.makeText(this,"Settings clicked",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.Contact_Us_menuItem  :
+                Toast.makeText(this,"Contact Selected",Toast.LENGTH_SHORT).show();
+                return true;
+
+                default:
+                    return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    private void setFragment(Fragment fragment)
+    {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.MainFrame,fragment);
+        fragmentTransaction.commit();
+
+    }
+}
---
+++ SolvedFragment.java
@@ -0,0 +1,33 @@
+package com.example.jiet_mainactivity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class SolvedFragment extends Fragment {
+
+
+    public SolvedFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_solved, container, false);
+    }
+
+
+}
