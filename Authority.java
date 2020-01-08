e9b5b235aaf552e2e804b102c221d20a32fcc0cb
asmita910 <55904719+asmita910@users.noreply.github.com>
2020-01-08 19:54:31 +0530

    please let me know if it need some changes or in case not correct

---
+++ Authority.java
@@ -0,0 +1,17 @@
+package com.example.jiet_mainactivity;
+
+public class Authority {
+    private String complaintId;
+
+    public Authority(String complaintId) {
+        this.complaintId = complaintId;
+    }
+
+    public String getComplaintId() {
+        return complaintId;
+    }
+
+    public void setComplaintId(String complaintId) {
+        this.complaintId = complaintId;
+    }
+}
